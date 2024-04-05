507ac69887d388e2002729c24484ae49909e2762
ElPokaReal <therealpoka211@gmail.com>
2024-04-04 23:03:59 -0400

    deleted:    .vscode/settings.json 	modified:   part1/courseinfo/src/App.jsx 	new file:   part1/courseinfo/src/components/Content.jsx 	new file:   part1/courseinfo/src/components/Header.jsx 	new file:   part1/courseinfo/src/components/Part.jsx 	new file:   part1/courseinfo/src/components/Total.jsx

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Total = (props) => {
+    return(
+      <div>
+        <p>Total exercises is: {props.total}</p>
+      </div>
+    )
+  }
+
+export default Total
